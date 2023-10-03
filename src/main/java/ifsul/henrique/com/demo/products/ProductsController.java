2543d10c82c3a026bc96c932eff837025b1fd332
Henrique Zardin de Lima <20182PF.CC0041@passofundo.ifsul.local>
2023-10-03 10:48:35 -0300

    inicio da criação da Configuração da Segurança

--- src/main/java/ifsul/henrique/com/demo/products/ProductsController.java
+++ src/main/java/ifsul/henrique/com/demo/products/ProductsController.java
@@ -4,6 +4,14 @@
  */
 package ifsul.henrique.com.demo.products;
 
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +24,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductsController {
     private final ProductsRepository productsRepository;
     
+    @Autowired
+    public ProductsController(ProductsRepository productsRepository) {
+        this.productsRepository = productsRepository;
+    }
     
+    @GetMapping("/{id}")
+    public ResponseEntity<EntityProducts> getEntityAById(@PathVariable Long id) {
+        Optional<EntityProducts> entityProducts = productsRepository.findById(id);
+        return entityProducts.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+    
+    @PostMapping
+    public ResponseEntity<EntityProducts> createEntityA(@RequestBody EntityProducts entityProducts) {
+        EntityProducts savedEntityProducts = productsRepository.save(entityProducts);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedEntityProducts);
+    }
 }
---
+++ src/main/java/ifsul/henrique/com/demo/products/Security/SecurityConfig.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ifsul.henrique.com.demo.products.Security;
+
+/**
+ *
+ * @author 20182PF.CC0041
+ */
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .authorizeRequests()
+                .antMatchers("/api/public/**").permitAll() // Rotas públicas
+                .anyRequest().authenticated()
+                .and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
