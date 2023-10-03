e092e5ef009dc50e2d5621158156b06176e4f9ae
Henrique Zardin de Lima <20182PF.CC0041@passofundo.ifsul.local>
2023-10-03 08:28:54 -0300

    inicio do trabalho

---
+++ src/test/java/ifsul/henrique/com/demo/LpeApplicationTests.java
@@ -0,0 +1,13 @@
+package ifsul.henrique.com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LpeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
